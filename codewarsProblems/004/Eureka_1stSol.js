3fc54101f23d245b1008e314fc4e9b4ad3ed6237
genesisparadox <genesisparadox@gmail.com>
2019-10-15 06:46:23 -0400

    new codewars problem + solution added

---
+++ codewarsProblems/004/Eureka_1stSol.js
@@ -0,0 +1,15 @@
+// Instruction - Eureka!!
+
+/*
+Take a number and sum its digits raised to the consecutive powers
+*/
+
+function sumDigPow(a, b) {
+  let arr = [];
+  let sum = 0;
+  for (let i=1; i<b; i++) {
+    sum+=Math(a,i);
+    a++;
+    if ()
+  }
+}
