0fcde86963a9e31eea62a38fb0b085491dc66ba2
DOEHOONLEE <genesisparadox@gmail.com>
2020-04-21 16:22:27 +0900

    Create 049_ReplaceWithAlphabetPosition.js

---
+++ codewarsProblems/JavaScript/6kyu/049_ReplaceWithAlphabetPosition.js
@@ -0,0 +1,28 @@
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+*/
+function alphabetPosition(text) {
+  var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var array = [];
+  text = text.toLowerCase();
+  for (var i = 0; i < text.length; i++) {
+    for (var x = 0; x < alpha.length; x++) {
+      if (text[i] == alpha[x]) {
+        var theNum = x+1;
+        array.push(theNum);
+      }
+    }
+  }
+  text = array.join().replace(/,/g , " ");
+  return text;
+}
